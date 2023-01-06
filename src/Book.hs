6e915b49b03ba0d3edf002b8602481e89c1f074f
Alexander Kobjolke <me@failco.de>
2023-01-06 23:39:17 +0100

    Implement Chapter 7 - Encoding

--- src/Book.hs
+++ src/Book.hs
@@ -22,10 +22,12 @@ import Control.Exception.Safe qualified as Ex
 
 import Data.ByteString qualified as BS
 import Data.ByteString.Lazy qualified as LBS
-import Data.Char qualified as Char
+import Data.ByteString.Builder qualified as BSB
+  
 import Data.Text qualified as T
 import Data.Text.Encoding qualified as T
 import Data.Text.IO qualified as T
+import Data.Time qualified as Time
 
 import Network.Socket (Socket)
 import Network.Socket qualified as S
@@ -150,11 +152,7 @@ openAndConnect addr = do
     S.connect s (S.addrAddress addr)
   pure (key, s)
 
-crlf :: [A.Char]
-crlf = [A.CarriageReturn, A.LineFeed]
-
--- HTTP data types
-
+-- Chapter 6 - HTTP data types
 data Request =
   Request RequestLine [HeaderField] (Maybe MessageBody)
   deriving (Eq, Show)
@@ -197,12 +195,103 @@ data HttpVersion = HttpVersion A.Digit A.Digit
   deriving (Eq, Show)
 
 -- excercise 16
-httpVersion :: HttpVersion
-httpVersion = HttpVersion Digit1 Digit1
+
+helloResponse :: Response
+helloResponse = Response statusLine [typ, len] (Just (MessageBody "Hello!"))
+  where
+    statusLine = StatusLine http_1_1 (StatusCode Digit2 Digit0 Digit0) (ReasonPhrase "OK")
+    typ = HeaderField (FieldName "Content-Type") (FieldValue "text/plain; charset=us-ascii")
+    len = HeaderField (FieldName "Content-Length") (FieldValue "6")
 
 helloRequest :: Request
 helloRequest = Request start [host, lang] Nothing
   where
-    start = RequestLine (Method [A.string|GET|]) (RequestTarget "/hello.txt") httpVersion
+    start = RequestLine (Method [A.string|GET|]) (RequestTarget "/hello.txt") http_1_1
     host = HeaderField (FieldName "Host") (FieldValue "www.example.com")
     lang = HeaderField (FieldName "Accept-Language") (FieldValue "en, mi")
+
+-- Chapter 7 - Encoding
+
+time :: IO () -> IO ()
+time action = do
+  a <- Time.getCurrentTime
+  action
+  b <- Time.getCurrentTime
+  print $ Time.diffUTCTime b a
+  
+crlf :: [A.Char]
+crlf = [A.CarriageReturn, A.LineFeed]
+
+http_1_1 :: HttpVersion
+http_1_1 = HttpVersion Digit1 Digit1
+
+encodeLineEnd :: BSB.Builder
+encodeLineEnd = A.lift crlf
+
+encodeRequest :: Request -> BSB.Builder
+encodeRequest (Request requestLine headerFields optionalBody) = mconcat
+  [ encodeRequestLine requestLine
+  , repeatedlyEncode encodeHeaderField headerFields
+  , encodeLineEnd
+  , optionallyEncode encodeMessageBody optionalBody
+  ]
+
+encodeRequestLine :: RequestLine -> BSB.Builder
+encodeRequestLine (RequestLine method target version) = mconcat
+  [ encodeMethod method
+  , A.lift [A.Space]
+  , encodeRequestTarget target
+  , A.lift [A.Space]
+  , encodeHttpVersion version
+  , encodeLineEnd
+  ]
+
+encodeMethod :: Method -> BSB.Builder
+encodeMethod (Method method) = BSB.byteString method
+
+encodeRequestTarget :: RequestTarget -> BSB.Builder
+encodeRequestTarget (RequestTarget target) = BSB.byteString target
+
+encodeHttpVersion :: HttpVersion -> BSB.Builder
+encodeHttpVersion (HttpVersion major minor) = mconcat
+  [ [A.string|HTTP/|]
+  , A.lift [major]
+  , [A.string|.|]
+  , A.lift [minor]
+  ]
+
+encodeMessageBody :: MessageBody -> BSB.Builder
+encodeMessageBody (MessageBody body) = BSB.lazyByteString body
+
+encodeHeaderField :: HeaderField -> BSB.Builder
+encodeHeaderField (HeaderField (FieldName name) (FieldValue value)) = BSB.byteString name <> A.lift [A.Colon, A.Space] <> BSB.byteString value <> encodeLineEnd
+
+repeatedlyEncode :: (a -> BSB.Builder) -> [a] -> BSB.Builder
+repeatedlyEncode = foldMap
+
+optionallyEncode :: (a -> BSB.Builder) -> Maybe a -> BSB.Builder
+optionallyEncode = foldMap
+
+encodeResponse :: Response -> BSB.Builder
+encodeResponse (Response statusLine headerFields optionalBody) = mconcat
+  [ encodeStatusLine statusLine
+  , encodeLineEnd
+  , repeatedlyEncode encodeHeaderField headerFields
+  , encodeLineEnd
+  , optionallyEncode encodeMessageBody optionalBody
+  ]
+
+encodeStatusLine :: StatusLine -> BSB.Builder
+encodeStatusLine (StatusLine version statusCode reasonPhrase) = mconcat
+  [ encodeHttpVersion version
+  , A.lift [A.Space]
+  , encodeStatusCode statusCode  
+  , A.lift [A.Space]
+  , encodeReasonPhrase reasonPhrase  
+  ]
+
+encodeStatusCode :: StatusCode -> BSB.Builder
+encodeStatusCode (StatusCode x y z) = A.lift [x, y, z]
+
+encodeReasonPhrase :: ReasonPhrase -> BSB.Builder
+encodeReasonPhrase (ReasonPhrase reasonPhrase) = BSB.byteString reasonPhrase
